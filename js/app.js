b45fc8c6b9bb4944a2a3d92d02f21846d39df9a2
Behtsa <behtsabet14@gmail.com>
2018-03-02 10:59:22 -0600

    add elements using template strings

--- js/app.js
+++ js/app.js
@@ -1,68 +1,60 @@
-const form = $('#search-form');
+const $form = $('#search-form');
 const $searchField = $('#search-keyword');
 const $responseContainer = $('#response-container');
 const $responseImg = $('#response-img');
 let $searchedForText;
 
-form.submit(function(e) {
-	e.preventDefault();
+loadPage = () => {
 	$responseContainer.html(" ");
 	$searchedForText = $searchField.val();
 	getPokemons();
-})
+};
 
 const handleError = () => {
 	console.log('Se ha presentado un error');
-}
+};
 
 const addPokemons = function(pokemons) {
 	const pokemonList = pokemons.pokemon_entries;
-	
+	console.log(pokemons)
+	let pokemonTs = ' ';
 
-	pokemonList.forEach(function(pokemon){
-		const name = pokemon.pokemon_species.name;
-		const src = "https://dummyimage.com/600x400/000/fff";
+	pokemonList.forEach(function(pokemon, index){
+		if(index < 50) {
+			// console.log(pokemon)
+		let name = pokemon.pokemon_species.name;
+		let url = pokemon.pokemon_species.url;
+		// const name = pokemon.pokemon_species.name;
+		const src = "https://dummyimage.com/100x100/000/fff";
 
-		let $li = document.createElement('li');
-		$li.className = 'articleClass';
-		$li.innerText = name;
+		pokemonTs += `<li>
+		<figure>
+		<img src = ${src}>
+		<figcaption>${name}</figcaption>
+		<span>${url}</span>
+		</figure>
+		</li>`
+		$responseContainer.html(pokemonTs);
+		//console.log($responseContainer)
+		}
+		// let $li = document.createElement('li');
+		// $li.className = 'articleClass';
+		// $li.innerText = name;
 
-		let $img = document.createElement('img');
-		// $img.attr('src', "https://dummyimage.com/600x400/000/fff");
-		$responseContainer.append($li);
+		// let $img = document.createElement('img');
+		// $img.src = src;
+		// $responseContainer.append($li);
 		// $responseContainer.append($img);
 	})
-	// // console.log(this);
-	// const data = JSON.parse(this.responseText);
-	// // console.log(data);
-	// const response = data.response.docs;
-	// console.log(response);
-
-	// response.forEach(element => {
-	// 	console.log(element);
-	// 	const title = element.headline.main;
-	// 	const snippet = element.snippet;
-	// 	// const src = element.multimedia[0].url;
-	// 	// console.log(src);
-
-	// 	let li = document.createElement('li');
-	// 	li.className = 'articleClass';
-	// 	li.innerText = snippet;
-
-	// 	responseContainer.appendChild(li);
-	// });
-
-	// let img = document.createElement('img');
-	// let info = img.setAttribute = ('src', src);
-	// console.log(info);
-	// responseContainer.appendChild(img);
-}
+};
 
 const getPokemons = () => {
 	$.ajax({
 		url : `https://pokeapi.co/api/v2/pokedex/1/`
 	}).done(addPokemons)
 	.fail(handleError);
-}
+};
+
+$(document).ready(loadPage);
 
 
